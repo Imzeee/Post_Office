6586145593c02064328a4c629ac70901f1fe0a15
Bartlomiej Mierzwa <bartek09144.1999@gmail.com>
2021-03-12 16:02:55 +0100

    code cleanup

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 
 
 pthread_mutex_t check_best_queue_mutex;
+pthread_mutex_t data_access_protecting_mutex;
 sem_t post_office_semaphore;
 sem_t worker_A_semaphore;
 sem_t worker_B_semaphore;
@@ -32,6 +33,11 @@ typedef struct Statistics_Data{
 
 Statistics_Data* stats;
 
+long int read_argument(int argc, char* argv[]);
+
+void initialize_mutex_and_semaphores();
+void release_mutex_and_semaphores();
+
 void* client_thread(void*);
 void* worker_A(void*);
 void* worker_B(void*);
@@ -41,8 +47,66 @@ char check_best_queue(Office_Windows*);
 void store_data(char);
 void print_statistics(void);
 
+void open_the_office(Office_Windows*, pthread_t*, long int );
 
 int main(int argc, char* argv[])
+{
+
+	long int number_of_clients = read_argument(argc,argv);
+	stats = (Statistics_Data*)calloc(3,sizeof(int)); // global structure for statistics
+	pthread_t* thread_id = (pthread_t*)malloc(number_of_clients*sizeof(pthread_t));
+	if(!thread_id)
+	{
+		perror("Memory allocation failed!\n");
+		exit(EXIT_FAILURE);
+	}
+	Office_Windows* Actual_Windows_Queue_Lengths = (Office_Windows*)calloc(3,sizeof(int));
+	if(!Actual_Windows_Queue_Lengths)
+	{
+		perror("Memory allocation failed!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	initialize_mutex_and_semaphores();
+
+	open_the_office(Actual_Windows_Queue_Lengths,thread_id,number_of_clients);
+
+	release_mutex_and_semaphores();
+
+	print_statistics();
+
+	free(thread_id);
+	free(Actual_Windows_Queue_Lengths);
+	free(stats);
+	return 0;
+}
+
+
+void open_the_office(Office_Windows* Actual_Windows_Queue_Lengths,pthread_t* thread_id, long int number_of_clients)
+{
+	int result;
+	for(int i=0;i<number_of_clients;i++)
+	{
+		result = pthread_create(&thread_id[i],NULL,client_thread,(void*)Actual_Windows_Queue_Lengths);
+		if(result!=0)
+		{
+			perror("Thread creation failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	for(int i=0;i<number_of_clients;i++)
+	{
+		result = pthread_join(thread_id[i],NULL);
+		if(result != 0)
+		{
+			perror("Thread join failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+
+long int read_argument(int argc, char* argv[])
 {
 	if(argc != 2)
 	{
@@ -69,22 +133,20 @@ int main(int argc, char* argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	pthread_t* thread_id = (pthread_t*)malloc(number_of_clients*sizeof(pthread_t));
-	if(!thread_id)
-	{
-		perror("Memory allocation failed!\n");
-		exit(EXIT_FAILURE);
-	}
-	Office_Windows* Actual_Windows_Queue_Lengths = (Office_Windows*)calloc(3,sizeof(int));
-	if(!Actual_Windows_Queue_Lengths)
-	{
-		perror("Memory allocation failed!\n");
-		exit(EXIT_FAILURE);
-	}
-	stats = (Statistics_Data*)calloc(3,sizeof(int));
+	return number_of_clients;
+}
 
+void initialize_mutex_and_semaphores()
+{
 	int result;
 	result = pthread_mutex_init(&check_best_queue_mutex,NULL);
+	if(result != 0)
+	{
+		perror("Mutex creation failed");
+		exit(EXIT_FAILURE);
+	}
+
+	result = pthread_mutex_init(&data_access_protecting_mutex,NULL);
 	if(result != 0)
 	{
 		perror("Mutex creation failed");
@@ -115,43 +177,19 @@ int main(int argc, char* argv[])
 		perror("Semaphore creation failed");
 		exit(EXIT_FAILURE);
 	}
+}
 
-
-	for(int i=0;i<number_of_clients;i++)
-	{
-		result = pthread_create(&thread_id[i],NULL,client_thread,(void*)Actual_Windows_Queue_Lengths);
-		if(result!=0)
-		{
-			perror("Thread creation failed");
-			exit(EXIT_FAILURE);
-		}
-	}
-	for(int i=0;i<number_of_clients;i++)
-	{
-		result = pthread_join(thread_id[i],NULL);
-		if(result != 0)
-		{
-			perror("Thread join failed");
-			exit(EXIT_FAILURE);
-		}
-	}
-
+void release_mutex_and_semaphores()
+{
 	sem_destroy(&post_office_semaphore);
 	sem_destroy(&worker_A_semaphore);
 	sem_destroy(&worker_B_semaphore);
 	sem_destroy(&worker_C_semaphore);
 	pthread_mutex_destroy(&check_best_queue_mutex);
-
-	print_statistics();
-
-	free(thread_id);
-	free(Actual_Windows_Queue_Lengths);
-	free(stats);
-
-	return 0;
+	pthread_mutex_destroy(&data_access_protecting_mutex);
 }
 
-void* client_thread(void* arg)
+void* client_thread(void* arg) // client behaviour simulation
 {
 	sem_wait(&post_office_semaphore);
 
@@ -229,6 +267,9 @@ void* client_thread(void* arg)
 
 void* worker_A(void* arg)
 {
+	// employee at window_A
+	// his work is simulated by sleep function
+	// one client can be handled at the time
 	Office_Windows* Actual_Windows_Queue_Lengths = (Office_Windows*)arg;
 	sem_wait(&worker_A_semaphore);
 	Actual_Windows_Queue_Lengths->current_window_A++;
@@ -236,11 +277,16 @@ void* worker_A(void* arg)
 	nanosleep(&task_doing_time,NULL);
 	sem_post(&worker_A_semaphore);
 	Actual_Windows_Queue_Lengths->current_window_A--;
+	pthread_mutex_lock(&data_access_protecting_mutex);
 	store_data('A');
+	pthread_mutex_unlock(&data_access_protecting_mutex);
 	pthread_exit(NULL);
 }
 void* worker_B(void* arg)
 {
+	// employee at window_B
+	// his work is simulated by sleep function
+	// one client can be handled at the time
 	Office_Windows* Actual_Windows_Queue_Lengths = (Office_Windows*)arg;
 	sem_wait(&worker_B_semaphore);
 	Actual_Windows_Queue_Lengths->current_window_B++;
@@ -248,11 +294,16 @@ void* worker_B(void* arg)
 	nanosleep(&task_doing_time,NULL);
 	sem_post(&worker_B_semaphore);
 	Actual_Windows_Queue_Lengths->current_window_B--;
+	pthread_mutex_lock(&data_access_protecting_mutex);
 	store_data('B');
+	pthread_mutex_unlock(&data_access_protecting_mutex);
 	pthread_exit(NULL);
 }
 void* worker_C(void* arg)
 {
+	// employee at window_C
+	// his work is simulated by sleep function
+	// one client can be handled at the time
 	Office_Windows* Actual_Windows_Queue_Lengths = (Office_Windows*)arg;
 	sem_wait(&worker_C_semaphore);
 	Actual_Windows_Queue_Lengths->current_window_C++;
@@ -260,36 +311,39 @@ void* worker_C(void* arg)
 	nanosleep(&task_doing_time,NULL);
 	sem_post(&worker_C_semaphore);
 	Actual_Windows_Queue_Lengths->current_window_C--;
+	pthread_mutex_lock(&data_access_protecting_mutex);
 	store_data('C');
+	pthread_mutex_unlock(&data_access_protecting_mutex);
+
 	pthread_exit(NULL);
 }
 
 char check_best_queue(Office_Windows* Actual_Windows_Queue_Lengths)
 {
-	// Algorithm to choose best queue
+	// Algorithm to choose best queue depending on actual number of clients in queue and handling time
 	int total_waiting_time_A = Actual_Windows_Queue_Lengths->current_window_A * WORKER_A_WORK_TIME;
 	int total_waiting_time_B = Actual_Windows_Queue_Lengths->current_window_B * WORKER_B_WORK_TIME;
 	int total_waiting_time_C = Actual_Windows_Queue_Lengths->current_window_C * WORKER_C_WORK_TIME;
 
 	if(total_waiting_time_C <= total_waiting_time_B && total_waiting_time_C <= total_waiting_time_A)
 	{
-		printf("Queue chosen: 3\n");
+		printf("Queue chosen by client: C\n");
 		return 'C'; //Worker_C
 	}
 	else if(total_waiting_time_B <= total_waiting_time_A && total_waiting_time_B <= total_waiting_time_C)
 	{
-		printf("Queue chosen: 2\n");
+		printf("Queue chosen by client: B\n");
 		return 'B'; //Worker_B
 	}
 	else
 	{
-		printf("Queue chosen: 1\n");
+		printf("Queue chosen by client: A\n");
 		return 'A'; // Worker_A
 	}
 
 }
 
-void store_data(char window_letter)
+void store_data(char window_letter) // one thread can access data at the time (memory safety)
 {
 	switch (window_letter)
 	{
@@ -308,12 +362,13 @@ void store_data(char window_letter)
 	}
 }
 
-void print_statistics(void)
+void print_statistics(void) // printing statistics after closing the post office
 {
-	printf("Statistics:\n");
-	printf("Window A: %d\n",stats->total_window_A);
-	printf("Window B: %d\n",stats->total_window_B);
-	printf("Window C: %d\n",stats->total_window_C);
+	printf("\n-----------\n");
+	printf("\nStatistics:\n");
+	printf("Window A: %d clients handled\n",stats->total_window_A);
+	printf("Window B: %d clients handled\n",stats->total_window_B);
+	printf("Window C: %d clients handled\n",stats->total_window_C);
 }
 
 
